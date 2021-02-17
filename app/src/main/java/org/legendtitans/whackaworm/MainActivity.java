5442ea37858ea960a57031ccdf52128a0754274d
legend-patrick <dstasiak@dcsdk12.org>
2021-02-17 11:26:52 -0700

    Added game images

--- app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
+++ app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
@@ -3,6 +3,11 @@ package org.legendtitans.whackaworm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageViewPlay = (ImageView) findViewById(R.id.playImageView);
+        imageViewPlay.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                Log.i("imageviewandontouchlistener", "playImageView onTouch");
+                return false;
+            }
+        });
     }
+
 }

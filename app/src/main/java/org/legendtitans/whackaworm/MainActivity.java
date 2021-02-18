8a73b0905f1618d1f0bc2e4284a58420481b30bd
legend-patrick <dstasiak@dcsdk12.org>
2021-02-18 12:16:02 -0700

    Added game images

--- app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
+++ app/src/main/java/org/legendtitans/whackaworm/MainActivity.java
@@ -2,6 +2,7 @@ package org.legendtitans.whackaworm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageViewPlay = (ImageView) findViewById(R.id.playImageView);
         imageViewPlay.setOnTouchListener(new View.OnTouchListener() {
-
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
-                Log.i("imageviewandontouchlistener", "playImageView onTouch");
+                //Update this code so you go to the next activity (worm gameboard)
+                Toast.makeText(getApplicationContext(), "I was touched", Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
     }
-
 }
